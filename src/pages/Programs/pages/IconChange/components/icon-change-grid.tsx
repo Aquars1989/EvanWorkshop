4f4dafc6f3121261314a3c696339a6c8b6c5d619
Evan.Hung <Evan.hung@dynapack.com.tw>
2023-05-15 13:40:52 +0800

    add message when init

--- src/pages/Programs/pages/IconChange/components/icon-change-grid.tsx
+++ src/pages/Programs/pages/IconChange/components/icon-change-grid.tsx
@@ -1,5 +1,6 @@
 import IconChangeItem from "./icon-change-item";
 import { v4 } from "uuid";
+import { FormattedMessage } from "react-intl";
 
 interface Props {
   listData: Array<any>;
@@ -10,7 +11,8 @@ export default function IconChangeGrid({ listData, setError }:Props) {
   //console.log(listData);
   return (
     <div className="row p-2 bg-secondary">
-      {listData.map((item) => {
+      {listData.length>0?
+      listData.map((item) => {
         const { id, word1, word2, word3 } = item;
         return (
           <div className="col-4 p-1 " key={v4()}>
@@ -23,7 +25,10 @@ export default function IconChangeGrid({ listData, setError }:Props) {
             />
           </div>
         );
-      })}
+      }):
+          <h4 className="text-white-50 text-center">
+            <FormattedMessage id={"iconChange.empty"} />
+          </h4>}
     </div>
   );
 }
