a49e9a0d299f8d6fa3aaf7f9573b1c114808d03a
Evan.Hung <Evan.hung@dynapack.com.tw>
2023-05-18 09:06:35 +0800

    reduce display delay time

--- src/pages/Programs/index.tsx
+++ src/pages/Programs/index.tsx
@@ -29,12 +29,10 @@ export default function Programs() {
   const cssTools = !inViewCrypto && (inViewScrabble||inViewIcon) ? "text-white":"text-white-50"
 
   const cardVariants = {
-    offscreen: {opacity: 0},
+    offscreen: {},
     onscreen: {
-      opacity: 1,
       transition: {
-        staggerChildren: 0.5,
-        delayChildren: 0.4,
+        delayChildren: 0.5,
       },
     }
   };
